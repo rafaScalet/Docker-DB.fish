1e89d500f8c5ca1052bbadde7b1cf2c6edb9b3f3
rafaScalet <rafaScalet@users.noreply.github.com>
2025-02-01 16:11:26 -0300

    add enviroment variable for connect on postgres

--- functions/postgres.fish
+++ functions/postgres.fish
@@ -1,12 +1,19 @@
 function postgres
+  set -l PG_PASS docker
+  set -l PG_USER docker
+  set -l PG_PORT 5432
+
   if not docker ps -a --format "{{.Names}}" | grep -q "postgres"
     docker run --name postgres \
-      -e POSTGRES_PASSWORD=docker \
-      -e POSTGRES_USER=docker \
-      -p 5432:5432 \
+      -e POSTGRES_PASSWORD=$PG_PASS \
+      -e POSTGRES_USER=$PG_USER \
+      -p $PG_PORT:5432 \
       -v postgres-data:/var/lib/postgresql/data \
       -d postgres > /dev/null
     echo "postgres container created"
+
+    set -Ux PG_CONNECTION "pg://$PG_USER:$PG_PASS@localhost:$PG_PORT/?sslmode=disable"
+    echo "PG_CONNECTION: $PG_CONNECTION"
     return
   end
 
@@ -15,8 +22,14 @@ function postgres
   if test "$postgres_status" = "true"
     echo "stopping postgres"
     docker stop postgres > /dev/null
+
+    set -e PG_CONNECTION
+    echo "PG_CONNECTION closed"
   else
     echo "starting postgres on port 5432"
     docker start postgres > /dev/null
+
+    set -Ux PG_CONNECTION "pg://$PG_USER:$PG_PASS@localhost:$PG_PORT/?sslmode=disable"
+    echo "PG_CONNECTION: $PG_CONNECTION"
   end
 end
